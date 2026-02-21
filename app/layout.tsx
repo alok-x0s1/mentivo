43a640aaa070da50f9d21fa59ef44b79fb88e842
Alok Yadav <yalok6321@gmail.com>
2026-02-21 09:44:34 +0530

    feat(schema): add seed script and tanstack query config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { QueryProvider } from "@/components/providers/query-provider";
 
-const josefinSans = Outfit({
+const outfit = Outfit({
 	variable: "--font-outfit",
 	subsets: ["latin"],
 	weight: ["300", "400", "500", "600", "700"],
@@ -23,8 +24,8 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={`${josefinSans.className} antialiased`}>
-					{children}
+				<body className={`${outfit.className} antialiased`}>
+					<QueryProvider>{children}</QueryProvider>
 				</body>
 			</html>
 		</ClerkProvider>
