ec30678f47cf7d121a778b0fff6eb00d4895c78b
Alok Yadav <yalok6321@gmail.com>
2026-03-20 10:37:11 +0530

    feat(chat): add conversations hook using tanstack

---
+++ hooks/use-conversations.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/api-client";
+
+export const useConversations = (conversationId: string) => {
+	return useQuery({
+		queryKey: ["messages", conversationId],
+		queryFn: async () => {
+			const res = await client.api.conversations[
+				":conversationId"
+			].messages.$get({
+				param: { conversationId },
+			});
+
+			if (!res.ok) {
+				throw new Error("Failed to fetch messages");
+			}
+
+			return res.json();
+		},
+
+		refetchInterval: 5000,
+	});
+};
